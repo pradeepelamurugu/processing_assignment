b13d3ac85c377d5fd7b4047540c1da214840d212
Pradeep Elamurugu <pradeepelamurugu2@gmail.com>
2024-04-21 21:28:54 +0530

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,8 @@ public class Main extends PApplet {
 
     private static final int WIDTH = 640;
     private static final int HEIGHT = 480;
-    public static final int DIAMETER = 50;
-    private int speed_of_ellipse_1 = 0, speed_of_ellipse_2 = 0, speed_of_ellipse_3 = 0, speed_of_ellipse_4 = 0;
+    private static final int DIAMETER = 50;
+    private Ellipse[] ellipses;
 
     public static void main(String[] args) {
         PApplet.main("Main", args);
@@ -15,23 +15,77 @@ public class Main extends PApplet {
     public void settings() {
         super.settings();
         size(WIDTH, HEIGHT);
+        ellipses = new Ellipse[4];
+        for (int i = 0; i < ellipses.length; i++) {
+            ellipses[i] = new Ellipse(1, (i + 1) * (HEIGHT / 5), DIAMETER);
+        }
     }
 
     @Override
     public void draw() {
-        moveEllipseFromLeftToRight();
+        for (int i = 0; i < ellipses.length; i++) {
+            ellipses[i].display();
+            ellipses[i].move(i + 1);
+        }
     }
 
-    private void moveEllipseFromLeftToRight() {
-        ellipse(speed_of_ellipse_1, (float) HEIGHT / 5, DIAMETER, DIAMETER);
-        speed_of_ellipse_1++;
-        ellipse(speed_of_ellipse_2, (float) HEIGHT / 5 * 2, DIAMETER, DIAMETER);
-        speed_of_ellipse_2 = speed_of_ellipse_1 * 2;
-        ellipse(speed_of_ellipse_3, (float) HEIGHT / 5 * 3, DIAMETER, DIAMETER);
-        speed_of_ellipse_3 = speed_of_ellipse_1 * 3;
-        ellipse(speed_of_ellipse_4, (float) HEIGHT / 5 * 4, DIAMETER, DIAMETER);
-        speed_of_ellipse_4 = speed_of_ellipse_1 * 4;
-    }
+    class Ellipse {
+        float x, y;
+        int diameter;
+        float speed;
 
+        Ellipse(float x, float y, int diameter) {
+            this.x = x;
+            this.y = y;
+            this.diameter = diameter;
+            this.speed = 1;
+        }
+
+        void display() {
+            ellipse(x, y, diameter, diameter);
+        }
+
+        void move(float speed) {
+            x += speed;
+        }
+    }
 
 }
+
+//import processing.core.PApplet;
+//
+//public class Main extends PApplet {
+//
+//    private static final int WIDTH = 640;
+//    private static final int HEIGHT = 480;
+//    public static final int DIAMETER = 50;
+//    private int speed_of_ellipse_1 = 0, speed_of_ellipse_2 = 0, speed_of_ellipse_3 = 0, speed_of_ellipse_4 = 0;
+//
+//    public static void main(String[] args) {
+//        PApplet.main("Main", args);
+//    }
+//
+//    @Override
+//    public void settings() {
+//        super.settings();
+//        size(WIDTH, HEIGHT);
+//    }
+//
+//    @Override
+//    public void draw() {
+//        moveEllipseFromLeftToRight();
+//    }
+//
+//    private void moveEllipseFromLeftToRight() {
+//        ellipse(speed_of_ellipse_1, (float) HEIGHT / 5, DIAMETER, DIAMETER);
+//        speed_of_ellipse_1++;
+//        ellipse(speed_of_ellipse_2, (float) HEIGHT / 5 * 2, DIAMETER, DIAMETER);
+//        speed_of_ellipse_2 = speed_of_ellipse_1 * 2;
+//        ellipse(speed_of_ellipse_3, (float) HEIGHT / 5 * 3, DIAMETER, DIAMETER);
+//        speed_of_ellipse_3 = speed_of_ellipse_1 * 3;
+//        ellipse(speed_of_ellipse_4, (float) HEIGHT / 5 * 4, DIAMETER, DIAMETER);
+//        speed_of_ellipse_4 = speed_of_ellipse_1 * 4;
+//    }
+//
+//
+//}
